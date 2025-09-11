f035413fd858f8bcad61ed50acb8e78d08e6195c
Joe <joevreug@gmail.com>
2025-09-11 15:56:12 +0200

    feat(match-analysis): add InsightsComponent and MatChipsModule to module declarations and imports

--- frontend/src/app/features/match-analysis/match-analysis.module.ts
+++ frontend/src/app/features/match-analysis/match-analysis.module.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatChipsModule } from "@angular/material/chips";
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
@@ -14,9 +15,16 @@ import { KillTimelineComponent } from "./components/kill-timeline/kill-timeline.
 import { MatchInputComponent } from "./components/match-input/match-input.component";
 import { PerformanceChartComponent } from "./components/performance-chart/performance-chart.component";
 import { MatchAnalysisComponent } from "./match-analysis.component";
+import { InsightsComponent } from "./components/insights/insights.component";
 
 @NgModule({
-  declarations: [MatchAnalysisComponent, MatchInputComponent, KillTimelineComponent, PerformanceChartComponent],
+  declarations: [
+    MatchAnalysisComponent,
+    MatchInputComponent,
+    KillTimelineComponent,
+    PerformanceChartComponent,
+    InsightsComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: "", component: MatchAnalysisComponent }]),
@@ -28,7 +36,8 @@ import { MatchAnalysisComponent } from "./match-analysis.component";
     MatProgressSpinnerModule,
     MatSnackBarModule,
     MatIconModule,
+    MatChipsModule,
   ],
-  exports: [MatchInputComponent, KillTimelineComponent, PerformanceChartComponent],
+  exports: [MatchInputComponent, KillTimelineComponent, PerformanceChartComponent, InsightsComponent],
 })
 export class MatchAnalysisModule {}
